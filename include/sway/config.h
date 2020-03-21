951e243755b73f4f339e2b5cd657721971b461fb
Michael Weiser <michael.weiser@gmx.de>
2020-03-21 23:54:55 +0100

    config: Remove unused background color
    Remove an unused struct member background in preparation for adding the
    structure to every container in an effort to mimise memory consumption.
    
    Signed-off-by: Michael Weiser <michael.weiser@gmx.de>

--- include/sway/config.h
+++ include/sway/config.h
@@ -521,7 +521,6 @@ struct sway_config {
 		struct border_colors unfocused;
 		struct border_colors urgent;
 		struct border_colors placeholder;
-		float background[4];
 	} border_colors;
 
 	// floating view
